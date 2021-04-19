3eda2aa12d59bf10b5d1cc81e3b420f160aee0c4
Abbos <yulchiyevabbos1@gamil.com>
2021-04-19 09:45:20 +0500

    #4

--- src/main/java/uz/pdp/apisecurityhrmanagement/repository/TourniquetHistoryRepository.java
+++ src/main/java/uz/pdp/apisecurityhrmanagement/repository/TourniquetHistoryRepository.java
@@ -1,9 +1,17 @@
 package uz.pdp.apisecurityhrmanagement.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import uz.pdp.apisecurityhrmanagement.entity.TourniquetHistory;
 
+import java.sql.Timestamp;
+import java.util.List;
 import java.util.UUID;
 
 public interface TourniquetHistoryRepository extends JpaRepository<TourniquetHistory, UUID> {
+
+    @Query(value = "select t.id from tournique_history t join users u on t.user_id = e.id \n" +
+            "where e.id =:userId and \n" +
+            "t.time between :from and :to", nativeQuery = true)
+    List<UUID> InAndOut(UUID userId, Timestamp from, Timestamp to);
 }
--- src/main/java/uz/pdp/apisecurityhrmanagement/repository/TourniquetRepository.java
+++ src/main/java/uz/pdp/apisecurityhrmanagement/repository/TourniquetRepository.java
@@ -1,9 +1,27 @@
 package uz.pdp.apisecurityhrmanagement.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import uz.pdp.apisecurityhrmanagement.entity.Task;
 import uz.pdp.apisecurityhrmanagement.entity.Tourniquet;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface TourniquetRepository extends JpaRepository<Tourniquet, Integer> {
+
+    Optional<Task> findAllByTaskTakerEmail(String taskTaker_email);
+
+    @Query(value = "select t.id from task t join user e on t.task_taker_id = e.id  " +
+            "where t.status='DONE' and e.id=:userId", nativeQuery = true)
+    List<UUID> completedTasks(UUID userId);
+
+    @Query(value = "select count(id) from task where task_taker_id =:userId",
+            nativeQuery = true)
+    Integer userAllTasks(UUID userId);
+
+    @Query(value = "select count(id) from task where task_taker_id =:userId " +
+            "and deadline>completed_date", nativeQuery = true)
+    Integer userCompletedTaskOnTime(UUID userId);
 }
